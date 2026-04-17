0a3abc8bb38cbdb7ffc52a3277da79ee8effb295
DJEBARRA Rabah Abderrazak <djebarra.ra@gmail.com>
2026-04-17 19:48:13 +0200

    Add PostgreSQL session store for production

--- src/config/session.js
+++ src/config/session.js
@@ -1,6 +1,24 @@
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const db = require('./database');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+let sessionStore;
+
+if (isProduction) {
+    // Production: use PostgreSQL session store
+    sessionStore = new pgSession({
+        pool: db.client,
+        tableName: 'session'
+    });
+} else {
+    // Development: use default memory store (fine for dev)
+    sessionStore = new session.MemoryStore();
+}
 
 module.exports = {
+    store: sessionStore,
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
